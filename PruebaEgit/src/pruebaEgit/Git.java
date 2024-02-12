a74e070cb1b7edcb8bd4e3a50703ba5905ed7e4f
LOUBIT <103092618+LucasOU21@users.noreply.github.com>
2024-02-12 10:49:08 +0100

    Update Git.java

--- PruebaEgit/src/pruebaEgit/Git.java
+++ PruebaEgit/src/pruebaEgit/Git.java
@@ -6,9 +6,7 @@ public class Git {
 	   System.out.println("PruebasEgit");
 	   System.out.println("Segundo commit máster");
 	   System.out.println("Primer commit rama 1");
-	   
 	   System.out.println("Tercer commit máster");
-
 	   System.out.println("Segundo commit rama 1");
 
 	}
