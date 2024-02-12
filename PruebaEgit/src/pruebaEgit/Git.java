7b62a5b853ac34ef4b86ef39f1108b9d512b92ba
LOU <LOU.0102@outlook.com>
2024-02-12 09:45:54 +0100

    Primer commit rama1

--- PruebaEgit/src/pruebaEgit/Git.java
+++ PruebaEgit/src/pruebaEgit/Git.java
@@ -5,6 +5,7 @@ public class Git {
 	public static void main(String[] args) {
 	   System.out.println("PruebasEgit");
 	   System.out.println("Segundo commit máster");
+	   System.out.println("Primer commit rama 1");
 	}
 
 }
